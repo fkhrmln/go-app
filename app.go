e378e9e03eeb7d7c0381393a7e36b2e148b44d23
Fakhri Maulana Ihsan <fakhrimaulanaihsan@gmail.com>
2024-05-21 22:42:10 +0700

    first commit

---
+++ app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gohello "github.com/fkhrmln/go-hello"
+)
+
+func main() {
+	fmt.Println(gohello.HelloWorld())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fkhrmln/go-app
+
+go 1.22.3
+
+require github.com/fkhrmln/go-hello v1.5.5
